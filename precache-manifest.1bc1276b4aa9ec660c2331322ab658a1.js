957fc50631fa83d5a7d2031925202a355a13a87f
PaulSarbajit <61593946+PaulSarbajit@users.noreply.github.com>
2020-03-17 21:24:48 +0530

    first commit

---
+++ precache-manifest.1bc1276b4aa9ec660c2331322ab658a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b467c333fd41322661bfb30f4347c8",
+    "url": "/PaulSarbajit/Shopping-Cart-App.git/index.html"
+  },
+  {
+    "revision": "807607fc0a495a75a750",
+    "url": "/PaulSarbajit/Shopping-Cart-App.git/static/css/main.68b6218e.chunk.css"
+  },
+  {
+    "revision": "31d5178741a745074416",
+    "url": "/PaulSarbajit/Shopping-Cart-App.git/static/js/2.8b283a95.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/PaulSarbajit/Shopping-Cart-App.git/static/js/2.8b283a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807607fc0a495a75a750",
+    "url": "/PaulSarbajit/Shopping-Cart-App.git/static/js/main.a97130d3.chunk.js"
+  },
+  {
+    "revision": "5af85f0a97331fde1691",
+    "url": "/PaulSarbajit/Shopping-Cart-App.git/static/js/runtime-main.3dd69116.js"
+  }
+]);
